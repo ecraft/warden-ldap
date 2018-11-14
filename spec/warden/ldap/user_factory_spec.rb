cbe8240a339eb7b435ab5522c36e41fa37c029d6
Olle Jonsson <olle.jonsson@gmail.com>
2018-11-14 21:42:53 +0100

    Spec: Add UserFactory test file

---
+++ spec/warden/ldap/user_factory_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RSpec.describe Warden::Ldap::UserFactory do
+  before do
+    Warden::Ldap.env = 'test'
+    Warden::Ldap.configure do |c|
+      c.config_file = File.join(__dir__, '../../fixtures/warden_ldap.yml')
+    end
+  end
+
+  subject { described_class.new(Warden::Ldap.configuration) }
+
+  describe '#initialize' do
+    it 'returns a UserFactory' do
+      expect(subject).to be_a(Warden::Ldap::UserFactory)
+    end
+  end
+
+  describe '#search' do
+    it 'returns nil if user not found'
+  end
+end
